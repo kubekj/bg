8baf71c9c32b9efa0b71f3775362bd9b271bf557
Jakub Jędraszka <s20193@pjwstk.edu.pl>
2022-11-10 13:14:17 +0000

    Merged PR 11: Initialize project -> .net 7.0 and next.js
    Related work items: #73, #74

---
+++ src/Domain/AssemblyReference.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+
+namespace Domain;
+
+public class AssemblyReference : Assembly { }
---
+++ tests/Architecture.Tests/ArchitectureTests.cs
@@ -0,0 +1,91 @@
+using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace Architecture.Tests;
+
+public class ArchitectureTests
+{
+    private const string ApplicationNamespace = "Application";
+    private const string InfrastructureNamespace = "Infrastructure";
+    private const string WebAppNamespace = "WebApp";
+    
+    [Fact]
+    public void Domain_Should_Not_Be_Dependent_On_Any_Project()
+    {
+        //Arrange
+        var domainAssembly = typeof(Domain.AssemblyReference).Assembly;
+
+        var otherProjects = new[]
+        {
+            ApplicationNamespace,
+            InfrastructureNamespace,
+            WebAppNamespace
+        };
+        
+        //Act
+        var sut = Types
+            .InAssembly(domainAssembly)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+        //Assert
+        sut.IsSuccessful.Should().BeTrue();
+    }
+    
+    [Fact]
+    public void Application_Should_Not_Be_Dependent_On_Infra_Or_WebApp()
+    {
+        //Arrange
+        var applicationAssembly = typeof(Application.AssemblyReference).Assembly;
+
+        var otherProjects = new[]
+        {
+            InfrastructureNamespace, WebAppNamespace
+        };
+        
+        //Act
+        var sut = Types
+            .InAssembly(applicationAssembly)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+        //Assert
+        sut.IsSuccessful.Should().BeTrue();
+    }
+    
+    [Fact]
+    public void Infrastructure_Should_Not_Be_Dependent_On_WebApp()
+    {
+        //Arrange
+        var infrastructureAssembly = typeof(Infrastructure.AssemblyReference).Assembly;
+
+        //Act
+        var sut = Types
+            .InAssembly(infrastructureAssembly)
+            .ShouldNot()
+            .HaveDependencyOn(WebAppNamespace)
+            .GetResult();
+
+        //Assert
+        sut.IsSuccessful.Should().BeTrue();
+    }
+    
+    [Fact]
+    public void WebApp_Should_Not_Be_Dependent_On_Infrastructure()
+    {
+        //Arrange
+        var webAppAssembly = typeof(WebApp.AssemblyReference).Assembly;
+
+        //Act
+        var sut = Types
+            .InAssembly(webAppAssembly)
+            .ShouldNot()
+            .HaveDependencyOn(InfrastructureNamespace)
+            .GetResult();
+
+        //Assert
+        sut.IsSuccessful.Should().BeTrue();
+    }
+}
