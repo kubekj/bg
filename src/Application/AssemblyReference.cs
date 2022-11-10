20bd4fdf710c51ecd77b72c83334bcb074fb7c9f
kubekj <kuba.jedraszka@gmail.com>
2022-11-10 13:45:41 +0100

    Finished setting up the architecture

---
+++ src/Application/AssemblyReference.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+
+namespace Application;
+
+public class AssemblyReference : Assembly { }
---
+++ src/Infrastructure/AssemblyReference.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+
+namespace Infrastructure;
+
+public class AssemblyReference : Assembly { }
