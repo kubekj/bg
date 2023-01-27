e01032eac8a80c8edbe866c25d9cefc44ecb22c7
kubekj <kuba.jedraszka@gmail.com>
2023-01-27 10:13:44 +0100

    again dtos

--- src/Application/DTO/Entities/TrainingPlanDto.cs
+++ src/Application/DTO/Entities/TrainingPlanDto.cs
@@ -1,3 +1,12 @@
 namespace Application.DTO.Entities;
 
-public record TrainingPlanDto();
+public record TrainingPlanDto(
+    string Title,
+    int Lenght,
+    int NoOfExercises,
+    string SkillLevel,
+    string Description,
+    string Language,
+    string CreatorEmail,
+    double RatingAvg,
+    IEnumerable<WorkoutDto> Workouts);
