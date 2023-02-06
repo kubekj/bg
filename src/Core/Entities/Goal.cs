809584082fd347380de0dcadf3a1fd3f1380fd38
Jakub Jędraszka <s20193@pjwstk.edu.pl>
2023-02-06 20:40:26 +0000

    Merged PR 81: Goal handling added

--- src/Core/Entities/Goal.cs
+++ src/Core/Entities/Goal.cs
@@ -11,8 +11,12 @@ public class Goal : Entity
         UserId = userId;
     }
 
+    public void EditGoal(int value)
+    {
+        Value = value;
+    }
     public int Month { get; }
-    public int Value { get; }
+    public int Value { get; private set; }
     public User User { get; private set; }
     public Guid UserId { get; private set; }
 }
--- src/Core/Repositories/IGoalRepository.cs
+++ src/Core/Repositories/IGoalRepository.cs
@@ -4,7 +4,7 @@ namespace Core.Repositories;
 
 public interface IGoalRepository
 {
-    public Task<int> GetByMonth(Guid userId);
+    public Task<Goal?> GetByMonth(Guid userId);
     public Task AddAsync(Goal goal);
     public Task EditAsync(Goal goal);
 }
